3458eeecb055a92963470910628a7a0cce11d201
suozhiwen <1050320769@qq.com>
2017-11-24 10:44:50 +0800

    DAO层接口

---
+++ src/main/java/team/runke/imm/dao/EventDetailsDao.java
@@ -0,0 +1,32 @@
+package team.runke.imm.dao;
+
+import team.runke.imm.entity.Employee;
+import team.runke.imm.entity.EventDetails;
+
+import java.util.List;
+/**
+ * @author 索志文
+ * @version 1.0
+ */
+public interface EventDetailsDao {
+    /**
+     * 指定条件查询活动详情
+     * @param eventDetails 活动详情
+     * @return
+     */
+    public List<EventDetails> selecrEventDetails(EventDetails eventDetails);
+
+    /**
+     *指定条件修改活动详情
+     * @param eventDetails
+     * @return
+     */
+    public int updateEventDetails(EventDetails eventDetails);
+
+    /**
+     *增加活动详情
+     * @param eventDetails
+     * @return
+     */
+    public int insertEventDetails(EventDetails eventDetails);
+}
---
+++ src/main/java/team/runke/imm/dao/PermissionsDao.java
@@ -0,0 +1,31 @@
+package team.runke.imm.dao;
+
+import team.runke.imm.entity.Permissions;
+
+import java.util.List;
+/**
+ * @author 索志文
+ * @version 1.0
+ */
+public interface PermissionsDao {
+    /**
+     * 指定条件权限
+     * @param permissions 权限信息
+     * @return
+     */
+    public List<Permissions> selecrPermissions(Permissions permissions);
+
+    /**
+     *指定条件修改权限信息
+     * @param permissions
+     * @return
+     */
+    public int updatePermissions(Permissions permissions);
+
+    /**
+     *增加权限
+     * @param permissions
+     * @return
+     */
+    public int insertPermissions(Permissions permissions);
+}
