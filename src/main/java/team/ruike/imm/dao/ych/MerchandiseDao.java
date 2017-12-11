6ec7547ae575e365a2b91aa119473b7180ed2981
yanchenhao <1>
2017-12-11 16:08:30 +0800

    test

---
+++ src/main/java/team/ruike/imm/dao/ych/MerchandiseDao.java
@@ -0,0 +1,37 @@
+package team.ruike.imm.dao.ych;
+
+import team.ruike.imm.entity.Merchandise;
+
+import java.util.List;
+
+/**
+ * @author 索志文
+ * @version 1.0
+ */
+public interface MerchandiseDao {
+    /**
+     * 指定条件查询商品信息
+     * @param merchandise 商品信息
+     * @return
+     */
+    public List<Merchandise> selecrMerchandise(Merchandise merchandise);
+
+    /**
+     *指定条件修改商品信息
+     * @param merchandise
+     * @return
+     */
+    public int updateMerchandise(Merchandise merchandise);
+
+    /**
+     *增加商品信息
+     * @param merchandise
+     * @return
+     */
+    public int insertMerchandise(Merchandise merchandise);
+    /**
+     * 分页
+     * @return
+     */
+    public List<Merchandise> PagerMerchandise(Merchandise merchandise);
+}
---
+++ src/main/java/team/ruike/imm/service/ych/MerchandiseController.java
@@ -0,0 +1,42 @@
+package team.ruike.imm.service.ych;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import team.ruike.imm.entity.Merchandise;
+import team.ruike.imm.service.ych.MerchandiseService;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * 闫琛昊
+ */
+@Controller
+@RequestMapping("merchandise")
+public class MerchandiseController {
+    @Autowired
+    MerchandiseService merchandiseService;
+
+    /**
+     * 查询数据
+     * @param merchandise
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/smerchandise.do")
+    public String select(Merchandise merchandise, HttpSession session){
+        List<Merchandise> merchandises = merchandiseService.selectMerchandise(merchandise);
+        session.setAttribute("merc", merchandises);
+        return "page/warehouse/goods-balance";
+    }
+    @RequestMapping(value = "/umerchandise.do")
+    public String update(Merchandise merchandise, HttpSession session){
+        int i =merchandiseService.updateMerchandise(merchandise);
+        if(i>0){
+            return "page/warehouse/goods-balance" ;
+        }
+        return "error";
+    }
+}
