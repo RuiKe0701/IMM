a8964026770e848c765b436ba79b783ab2f3616d
yanchenhao <1>
2017-12-12 15:21:19 +0800

    asd

--- src/main/webapp/js/warehouse.js
+++ src/main/webapp/js/warehouse.js
@@ -43,7 +43,7 @@ $(function () {
         $("li").removeClass("l-selected");
         $("div").removeClass("l-tab-links-item-close");
         // $("#iframs").before().append("<li tabid=\"purchase-purchaseOrder\" class=\"l-selected\"><a>销货订单</a><div class=\"l-tab-links-item-left\"></div><div class=\"l-tab-links-item-right\"></div><div id='aaa' class=\"l-tab-links-item-close\"></div></li> <li id=\"tabManage\"></li>");
-        $("#iff").attr("src", "/warehouseReport/goods-balance.do");
+        $("#iff").attr("src", "/merchandise/smerchandise.do");
 
         $("#ons").css("display", "none");
         $("#showiframe").css("display", "block");
